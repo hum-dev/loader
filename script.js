5de06b7a69c6d929ec7975a20703ff2c0dbeed4a
Humphrey Muriungi <kymh21@outlook.com>
2022-07-10 22:39:03 +0300

    form validation

---
+++ script.js
@@ -0,0 +1,8 @@
+var form = document.getElementById('form')
+
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+
+    var username = document.getElementById('username').value
+    var email = document.getElementById('email').value
+})
